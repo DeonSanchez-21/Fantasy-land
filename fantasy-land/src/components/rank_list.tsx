58be22a705159e9f212cbc22a320e12ee2ecba75
Deon <dsanchez6@gmail.com>
2025-01-03 05:50:13 -0500

    commit

--- fantasy-land/src/components/rank_list.tsx
+++ fantasy-land/src/components/rank_list.tsx
@@ -1,29 +1,50 @@
-import { Avatar, IconButton, List, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
-import FolderIcon from '@mui/icons-material/Folder';
-import DeleteIcon from '@mui/icons-material/Delete';
-import React from 'react'
+import { Avatar, Box, List, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import henry from '../assets/henry.jpg';
+import { fetchData } from '../utility/fetch';
+import { useEffect } from 'react';
+
+const getPlayerData = async () => {
+  const allPlayers = await fetchData("https://api.sleeper.app/v1/players/nfl");
+  const players = Object.keys(allPlayers).map(function(key) {
+    return allPlayers[key]
+  })
+
+  const activePlayers = await Promise.all(players?.filter(async(player: any) => { player.active = true || player.first_name != "Duplicate" }).sort((a, b) => a.search_rank - b.search_rank));
+  
+
+  console.log(activePlayers)
+  return activePlayers;
+}
+
+const list = [0,1,2,3,4];
+
+const PlayerCard = () =>{
+  return (
+    <ListItem>
+        <ListItemAvatar>
+            <Avatar alt='Derek Henry' src={henry}/>
+        </ListItemAvatar>
+
+        <ListItemText
+          primary="Derek Henry"
+          secondary={'Secondary text' }
+          />
+    </ListItem>
+  )
+}
 
 const Rank_list = () => {
+  
+  useEffect(() =>{getPlayerData()},[])
+
   return (
-    <List>
-        <ListItem
-            secondaryAction={
-            <IconButton edge="end" aria-label="delete">
-                <DeleteIcon />
-            </IconButton>
-            }
-        >
-            <ListItemAvatar>
-            <Avatar>
-                <FolderIcon />
-            </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-            primary="Single-line item"
-            secondary={'Secondary text' }
-            />
-        </ListItem>
-    </List>
+    <Box sx={{width:'50%', m:'auto', bgcolor: 'white'}}>
+      <List sx={{outline:'1px solid red', width:'50%', m:'auto', minHeight:'100vh' }}>
+          {list.map((element) => {
+            return <PlayerCard/>
+          })}
+      </List>
+    </Box>
   )
 }
 
--- fantasy-land/src/pages/home.tsx
+++ fantasy-land/src/pages/home.tsx
@@ -2,9 +2,10 @@ import { Box } from "@mui/material"
 import Rank_list from "../components/rank_list"
 
 
+
 const Home = () => {
   return (
-    <Box >
+    <Box sx={{backgroundColor:'skyblue', minHeight:'100vh', m:0}}>
       <Rank_list/>
     </Box>
   )
